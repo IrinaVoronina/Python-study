5dbafbc92f2b657891f4450f435e5d7b10edc14d
ivoronina <doorinclouds@mail.ru>
2022-11-10 20:53:44 +0300

    Commit

--- Telebot/main.py
+++ Telebot/main.py
@@ -1,5 +1,9 @@
 import random
 import time
+
+import markup as markup
+from aiogram.types import reply_keyboard, ReplyKeyboardMarkup, update
+
 from config import tg_bot_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -8,9 +12,16 @@ from aiogram.utils import executor
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
+def start(bot, update):
+    update.message.reply_text('Hello, I`m smart password generator. I can help you to create safety password.',reply_markup=markup)
+
+reply_keyboard = [["/start"]]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await message.answer("Hello, how many symbols of password do you want?")
+    await message.answer("Hello, I`m smart password generator. I can help you to create safety password.")
+    time.sleep(3)
+    await message.answer("How many symbols of password do you want to create?")
 
 @dp.message_handler()
 async def get_password(message: types.Message):
@@ -18,15 +29,19 @@ async def get_password(message: types.Message):
     try:
         passlength = int(passlength)
         if passlength > 15 or passlength < 5:
-            await message.reply("Inappropriate length of password")
+            await message.reply("We recommend to choose from 5 to 12 symbols")
         else:
             a = "abcdefghijklmnopqrstuvwxyz"
             b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             с = "0123456789"
-            d = "{}[]()?!*,-_"
+            d = "{}[]()?!*,-_$%#&@"
             all = a + b + с + d
             password = "".join(random.sample(all, passlength))
+            await message.answer("Generation is going...")
+            time.sleep(1)
             await message.answer(f"Your password: {password}")
+            time.sleep(2)
+            await message.answer("Would you like to create one more password? :)")
     except Exception as ex2:
         print(ex2)
         await message.answer("Enter the number of symbols from 5 to 15")
